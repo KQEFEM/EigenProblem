9a45e4bfa41a8fe31c45b5ab973311ba1b566292
Kieran Quaine <kieran.s.quaine@btinternet.com>
2024-12-03 14:37:03 +0100

    test code

--- eigenproblem.py
+++ eigenproblem.py
@@ -17,14 +17,27 @@ from mpi4py import MPI
 import numpy as np
 
 import ufl
+import MeshingCodes as meshing
 from dolfinx import fem, io, mesh, plot
+from dolfinx.mesh import create_box
 from dolfinx.fem.petsc import LinearProblem
+
 from ufl import ds, dx, grad, inner
 
-msh = mesh.create_rectangle(
+
+msh1 = create_box(
     comm=MPI.COMM_WORLD,
-    points=((0.0, 0.0), (2.0, 1.0)),
-    n=(32, 16),
-    cell_type=mesh.CellType.triangle,
+    points=[(0.0, 0.0, 0.0), (3.0, 2.0, 1.0)],
+    n=[30, 20, 10],
+    cell_type=mesh.CellType.tetrahedron,
 )
-V = fem.functionspace(msh, ("Lagrange", 1))
+
+msh1 = meshing.main()
+
+print(msh1)
+
+print(type(msh1))
+element = ufl.finiteelement("N1curl", msh1.ufl_cell(), 1)
+V = fem.FunctionSpace(msh1, element)# Define basis and bilinear form
+u = fem.TrialFunction(V)
+v = fem.TestFunction(V)
