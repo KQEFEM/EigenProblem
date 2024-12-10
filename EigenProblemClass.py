5304cb0d9070270004cfe757f0eea288a6e61872
Kieran Quaine <kieran.s.quaine@btinternet.com>
2024-12-10 14:31:15 +0100

    updated comments

--- EigenProblemClass.py
+++ EigenProblemClass.py
@@ -39,6 +39,10 @@ class FENicSEigenProblem:
         Docker command to start fenics
         docker run -ti -v $(pwd):/home/fenics/shared -w /home/fenics/shared quay.io/fenicsproject/stable:current
 
+    The solver uses SLEPc and is based on the tutorial: https://docs.fenicsproject.org/dolfinx/main/python/demos/demo_half_loaded_waveguide.html
+    See https://slepc.upv.es/documentation/slepc.pdf for more information on SLEPc
+
+    #? The test mode is not set-up yet.
     """
 
     def __init__(
@@ -51,13 +55,13 @@ class FENicSEigenProblem:
     ):
         """
 
-          Args:
-            num_nodes (int): Number of nodes for mesh generation (default: 100).
-            domain_type (str): Type of domain (e.g., 'cube', 'rectangle').
-            test_problem (bool): Run a predefined test problem (default: False).
-            test_mode (bool): Enable testing mode for pytest (default: False).
-            num_eigenvalues (int): Number of eigenvalues to compute (default: 10).
-      
+        Args:
+          num_nodes (int): Number of nodes for mesh generation (default: 100).
+          domain_type (str): Type of domain (e.g., 'cube', 'rectangle').
+          test_problem (bool): Run a predefined test problem (default: False).
+          test_mode (bool): Enable testing mode for pytest (default: False).
+          num_eigenvalues (int): Number of eigenvalues to compute (default: 10).
+
         """
         self.num_nodes = num_nodes
         self.domain_type = domain_type
@@ -85,13 +89,42 @@ class FENicSEigenProblem:
         self.script_dir = os.path.dirname(os.path.realpath(__file__))
 
     def set_constants(self):
-        self.frequency = fem.Constant(self.mesh, 1.0)  # omega = 0 for unexcited system
-        self.permittivity = fem.Constant(self.mesh, 1.0)  # epsilon
-        self.permeability = fem.Constant(self.mesh, 1.0)  # mu
+        """
+        Set the physical constants for the problem.
+
+        This method initializes the frequency, permittivity, and permeability
+        constants used in the Maxwell eigenvalue problem.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        # Frequency of the system (omega)
+        self.frequency = fem.Constant(self.mesh, 1.0)
+
+        # Electric permittivity (epsilon)
+        self.permittivity = fem.Constant(self.mesh, 1.0)
+
+        # Magnetic permeability (mu)
+        self.permeability = fem.Constant(self.mesh, 1.0)
 
     def create_mesh_and_function_space(self):
-        """Creates the mesh for the problem. This is either a rectangle or a circle."""
+        """Creates a mesh for the problem.
+
+        The mesh type is selected based on the value of the `domain_type` parameter. The
+        mesh is either a rectangle or a cube, and the number of nodes is specified by the
+        `num_nodes` parameter.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
         if self.domain_type.lower() == "rectangle":
+            # Create a rectangle mesh
             self.mesh = fem.RectangleMesh(
                 fem.Point(0, 0),
                 fem.Point(self.domain[0], self.domain[1]),
@@ -99,6 +132,7 @@ class FENicSEigenProblem:
                 self.num_nodes,
             )
         elif self.domain_type.lower() == "cube":
+            # Create a cube mesh
             self.domain = [1, 1, 1]
             self.num_nodes = 10
             print("Domain:", self.domain)
@@ -114,29 +148,28 @@ class FENicSEigenProblem:
             )
         else:
             print("\nStandard unit square is used.\n")
+            # Create a unit square mesh
             self.mesh = fem.UnitSquareMesh(self.num_nodes, self.num_nodes)
 
+        # Create the function space
         self.V = fem.functionspace(self.mesh, ("P", 1))
         self.u = ufl.TrialFunction(self.V)
         self.v = ufl.TestFunction(self.V)
 
-    def RHS_test_problem(self):
-        """
-        Define the right-hand side function for the equation. for u(x,y) = sin(x)sin(y), the right-hand side function is -2sin(x)sin(y)
-
-        Parameters:
-        degree (int): The degree of polynomial approximation used when the expression
-                    is interpolated onto a finite element function space. It should
-                    match the degree of the finite element space where this expression
-                    will be used. For example, for linear or "P1" elements, use degree=1;
-                    for quadratic or "P2" elements, use degree=2.
-        """
-        self.rhs_func = -fem.Expression("-2*sin(x[0])*sin(x[1])", degree=1)
-
     def weak_form(self):
-        """Computes the weak form of the problem."""
+        """Computes the weak form of the Maxwell eigenvalue problem.
 
+        This method defines the weak form by calculating the curl-curl
+        and mass bilinear forms, then combines them to form the left-hand
+        side (LHS) of the equation. The right-hand side (RHS) is set to zero.
+
+        \nabla \times \nabla \times E - \omega^2 \varepsilon \mu E = 0
+        """
+
+        # Define the curl-curl bilinear form
         curl_curl = ufl.dot(ufl.curl(self.u), ufl.curl(self.v)) * ufl.dx
+
+        # Define the mass bilinear form
         mass = (
             self.frequency**2
             * self.permittivity
@@ -144,25 +177,43 @@ class FENicSEigenProblem:
             * ufl.dot(self.u, self.v)
             * ufl.dx
         )
+
+        # Combine the curl-curl and mass bilinear forms for the LHS
         self.LHS = curl_curl + mass
+
+        # Set the RHS to zero
         self.RHS = 0
 
-    def boundary_condition(self):
+    def set_boundary_condition(self):
         """Create a Dirichlet boundary condition on the entire boundary.
-        Parameters: V: FunctionSpace - The function space to apply the boundary condition to. value: float - The value of the Dirichlet condition (default is 0.0).
-        Returns: bc: DirichletBC - The boundary condition."""
-        # Define the value of the boundary condition
+
+        Parameters
+        ----------
+        value : float
+            The value of the Dirichlet condition (default is 0.0).
+
+        Returns
+        -------
+        boundary_condition : DirichletBC
+            The boundary condition.
+        """
         value = 0.0
-        u_D = fem.Constant(self.V.mesh, value)
-        # Locate the degrees of freedom on the boundary
+        bc_function = fem.Constant(self.V.mesh, value)
+
         boundary_dofs = fem.locate_dofs_geometrical(
             self.V, lambda x: np.full(x.shape[1], True)
-        )
-        # Create the boundary condition
-        self.bc = [fem.dirichletbc(u_D, boundary_dofs, self.V)]  # Wrap in list
+        )  # All boundary dofs
+
+        self.bc = [fem.dirichletbc(bc_function, boundary_dofs, self.V)]  # Wrap in list
 
     def solve_eigenvalue_problem(self):
-        """Solves the eigenvalue problem for the bilinear form using SLEPc."""
+        """
+        Solves the eigenvalue problem for the bilinear form using SLEPc.
+        eps.setDimensions(nev=10, ncv=20, mpd=15)
+        Specify the number of eigenvalues to compute
+        mpd (Maximum Projected Dimension): The maximum size of the search subspace. mpd should be less than or equal to ncv.
+        ncv (Number of Converged Values): The number of basis vectors in the eigenspace. ncv should be greater than nev.
+        """
         # Create a solution function in the function space
         self.solution = fem.Function(self.V)
 
@@ -171,15 +222,12 @@ class FENicSEigenProblem:
         A.assemble()
         self.eps = SLEPc.EPS().create(self.mesh.comm)  # This represents the solver
         self.eps.setOperators(A)  # Set the operators for the eigenvalue problem
-        """ If the matrices in the problem have known properties (e.g. hermiticity) we can use this information in SLEPc to accelerate the calculation with the setProblemType function. For this problem, there is no property that can be exploited, and therefore we define it as a generalized non-Hermitian eigenvalue problem with the SLEPc.EPS.ProblemType.GNHEP object """
+        """ 
+        If the matrices in the problem have known properties (e.g. hermiticity) we can use this information in SLEPc to accelerate the calculation with the setProblemType function. For this problem, there is no property that can be exploited, and therefore we define it as a generalized non-Hermitian eigenvalue problem with the SLEPc.EPS.ProblemType.GNHEP object 
+        """
         self.eps.setProblemType(SLEPc.EPS.ProblemType.GNHEP)
 
-        """ Specify the number of eigenvalues to compute
-        eps.setDimensions(nev=10, ncv=20, mpd=15)
-        mpd (Maximum Projected Dimension): The maximum size of the search subspace. mpd should be less than or equal to ncv.
-        ncv (Number of Converged Values): The number of basis vectors in the eigenspace. ncv should be greater than nev.
-        """
-        self.eps.setDimensions(nev=self.num_eigenvalues)  # Request 10 eigenvalues
+        self.eps.setDimensions(nev=self.num_eigenvalues)
 
         self.eps.setTolerances(tol=self.tol)
 
@@ -195,24 +243,15 @@ class FENicSEigenProblem:
         nconv = self.eps.getConverged()
         print(f"Number of converged eigenvalues: {nconv}")
 
-        # Get the eigenvalues and eigenvectors
-        # print("done")
-
-    def solve_problem(self):
-        """Solves the bilinear form."""
-        self.solution = fem.Function(self.V)
-        a = ufl.lhs(self.LHS)
-        if self.RHS != 0:
-            L = ufl.rhs(self.RHS)
-
-            fem.solve(a == L, self.u, self.bc)
-        else:
-            problem = fem.fem.petscLinearProblem(a, self.u, self.bc)
-            uh = problem.solve()
-
-        print("solved")
-
     def save_eigenproblem(self):
+        """
+        Saves the eigenvalues to a .pkl file. The eigenvalues are sorted by
+        their real part.
+
+        The file is saved in the 'data' folder of the script directory with
+        the name 'eigenvalues_nX.pkl', where X is the number of eigenvalues
+        computed.
+        """
         # Save the eigenvalues to a .pkl file
         self.vals = [
             (i, np.sqrt(-self.eps.getEigenvalue(i)))
@@ -242,11 +281,9 @@ class FENicSEigenProblem:
         print("\n\n Computed mesh and function space \n\n ")
         self.set_constants()
         self.weak_form()
-        self.boundary_condition()
+        self.set_boundary_condition()
         self.solve_eigenvalue_problem()
         self.save_eigenproblem()
-        # self.solve_problem()
-        # self.save_solution()
 
 
 class BoundaryFunction:
@@ -268,7 +305,9 @@ def main():
 
 
 def test():
-    """This is for running the doc test"""
+    """
+    #! This is for running the doc test and does not work yet
+    """
     # Delete the test_run folder if it exists
     output_dir_test = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "test_run"
